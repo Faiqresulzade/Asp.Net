efce27d5d5160d097ba4a96b48062d0c69cf1e65
Faiqresulzade <faigbr@code.edu.az>
2022-10-05 12:39:13 +0400

    05.10Homework

---
+++ front-to-back/Controllers/AboutController.cs
@@ -0,0 +1,24 @@
+﻿using front_to_back.Models;
+using front_to_back.ViewModels.About;
+using Microsoft.AspNetCore.Mvc;
+
+namespace front_to_back.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            var ourVision = new List<OurVision>
+            {
+                new OurVision{Title="Our Vision ", Text="Incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse commodo viverra.",FilePath="display-4 bx bxs-bulb text-light"},
+                new OurVision{Title="Our Mission",Text="Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis.",FilePath="display-4 bx bx-revision text-light"},
+                new OurVision{Title="Our Goal",Text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor." ,FilePath="display-4 bx bxs-select-multiple text-light"}
+            };
+            var model=new AboutIndexViewModels 
+            { 
+                ourVisions = ourVision
+            };
+            return View(model);
+        }
+    }
+}
---
+++ front-to-back/ViewModels/About/AboutIndexViewModels.cs
@@ -0,0 +1,10 @@
+﻿using front_to_back.Models;
+
+namespace front_to_back.ViewModels.About
+{
+    public class AboutIndexViewModels
+    {
+        public List<OurVision> ourVisions { get; set; }
+        public List<OurVision> FilePath { get; set; }
+    }
+}
